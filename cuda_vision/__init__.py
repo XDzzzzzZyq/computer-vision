84b7902849fb89012357fce56200974b99d9e9fd
XDzZyq <xiaodouzizyq@gmail.com>
2024-11-15 17:45:36 +0800

    11.15 random dithering with different distribution

--- cuda_vision/__init__.py
+++ cuda_vision/__init__.py
@@ -3,12 +3,14 @@ if __name__ == "__main__":
     from cuda_vision import filters, convert
 
     img = load_raw("../imgs/barbara.raw", 256, 256, 1)
-    h1 = convert.hash(img, 1)
-    h2 = convert.hash(img, 2)
-    h3 = convert.hash(img, 3)
-    compare_imgs([img, h1, h3])
-    compare_hist([h1, h2, h3])
+    bin1 = convert.random_threshold(img, "uniform", 00, seed=123)
+    bin2 = convert.random_threshold(img, "uniform", 10, seed=123)
+    bin3 = convert.random_threshold(img, "uniform", 30, seed=123)
+    bin4 = convert.random_threshold(img, "uniform", 50, seed=123)
+    compare_imgs([img, bin1, bin2, bin3, bin4])
+    bin1 = convert.random_threshold(img, "gaussian", 00, seed=123)
+    bin2 = convert.random_threshold(img, "gaussian", 10, seed=123)
+    bin3 = convert.random_threshold(img, "gaussian", 30, seed=123)
+    bin4 = convert.random_threshold(img, "gaussian", 50, seed=123)
+    compare_imgs([img, bin1, bin2, bin3, bin4])
     plt.show()
-
-
-
--- cuda_vision/convert.py
+++ cuda_vision/convert.py
@@ -23,7 +23,13 @@ def threshold(img: torch, threshold) -> torch.Tensor:
     return _convert.threshold(img, threshold)
 
 
-def random_threshold(img: torch, ratio=1.0, seed=123, step=2) -> torch.Tensor:
-    noise = hash(img, step=step, seed=seed)
-    noise = (noise-127.5) * ratio + 127.5
+def random_threshold(img: torch, type, std=127.5, seed=123, step=2) -> torch.Tensor:
+    if type == 'uniform':
+        noise = hash(img, step=step, seed=seed)
+        noise = (noise-127.5) * std*(12**0.5)/255.0 + 127.5
+    elif type == 'gaussian':
+        noise = torch.randn_like(img) * std + 127.5
+        noise = torch.clamp(noise, 0.0, 255.0)
+    else:
+        noise = torch.randn_like(img)
     return _convert.random_threshold(img, noise)
