2d2ba5108b64e8355dcde07e21ba47e89bfb40f4
XDzZyq <xiaodouzizyq@gmail.com>
2024-11-27 10:57:28 +0800

    11.27 rename

--- cuda_vision/feature.py
+++ cuda_vision/feature.py
@@ -6,10 +6,8 @@ from cuda_vision.__compile import load_src
 _stats = load_src("stats")
 
 
-def get_momentum(imgs: torch.Tensor, order=4, window=4) -> torch.Tensor:
-    sat = _stats.to_sat(imgs, order)
-    return sat
-
+def get_sat(imgs: torch.Tensor, order=4) -> torch.Tensor:
+    return _stats.to_sat(imgs, order)
 
 
 if __name__ == "__main__":
@@ -18,7 +16,4 @@ if __name__ == "__main__":
     tex = load_raw('../imgs/comb1.raw', 256, 256, 1)
     a = _stats.to_sat(tex, 4)
     compare_imgs([a[:,0:1], a[:,1:2]], range=None)
-    print(a[:,0:1])
-    print(a[:,1:2])
-    print(tex)
     plt.show()
