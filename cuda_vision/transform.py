c31f7c2a1112515b8f6d0826daf73afcdfb12aff
XDzZyq <xiaodouzizyq@gmail.com>
2024-11-29 21:31:57 +0800

    11.29 segmentation

--- cuda_vision/transform.py
+++ cuda_vision/transform.py
@@ -33,6 +33,31 @@ def scale(imgs: torch.Tensor, ratio: tuple[float, float] or float) -> torch.Tens
     return imgs
 
 
+def island_segment(imgs: torch.Tensor) -> list[torch.Tensor]:
+    from scipy.ndimage import label
+    B, _, H, W = imgs.shape
+    labeled_image, num_features = label(imgs.cpu())
+    labeled_image = torch.from_numpy(labeled_image).float()
+    islands = [(labeled_image == label).float().to(imgs.device) for label in range(1, num_features+1)]
+
+    def get_aabb(island):
+        _, _, cols, rows = island.nonzero(as_tuple=True)
+        y_min, y_max = rows.min().item(), rows.max().item()
+        x_min, x_max = cols.min().item(), cols.max().item()
+        return x_min, x_max+1, y_min, y_max+1
+
+    coord = [get_aabb(island) for island in islands]
+    segments = []
+    for c, island in zip(coord, islands):
+        x_min, x_max, y_min, y_max = c
+        offset = (W/2 - (x_max+x_min)/2, H/2 - (y_max+y_min)/2)
+        ratio = (W/(x_max-x_min), H/(y_max-y_min))
+        island = translate(island, offset=offset)
+        island = scale(island, ratio=ratio)
+        segments.append(island)
+    return segments
+
+
 def custom_transform(imgs: torch.Tensor, trans: torch.Tensor or Transform) -> torch.Tensor:
 
     if isinstance(trans, Transform):
@@ -58,11 +83,11 @@ def disk_warp(img: torch.Tensor, inverse=False) -> torch.Tensor:
 
 if __name__ == "__main__":
     from utils.imageIO import *
+    from convert import invert
     import math
 
-    img = load_raw('../imgs/baboon.raw', 512, 512, 1)
-    w = disk_warp(img)
-    r = disk_warp(w, inverse=True)
-
-    compare_imgs([img, w, r])
+    train = load_raw('../imgs/training.raw', 256, 256, 3)[:,0:1].contiguous()
+    train = invert(train)
+    seg = island_segment(train)
+    compare_imgs([train]+seg, range=None)
     plt.show()
