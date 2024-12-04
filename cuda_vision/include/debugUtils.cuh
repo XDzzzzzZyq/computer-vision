2d549e78479c47aab5bd7b8b8b4d135da1cf84d8
XDzZyq <xiaodouzizyq@gmail.com>
2024-12-04 12:02:20 +0800

    12.4 debug cuda config

--- cuda_vision/include/debugUtils.cuh
+++ cuda_vision/include/debugUtils.cuh
@@ -28,3 +28,36 @@ void check_error() {
         std::cout << "Kernel launch failed: " << cudaGetErrorString(err) << std::endl;
     }
 }
+
+void check_max_grid_size() {
+    int device_count = 0;
+    cudaGetDeviceCount(&device_count);
+
+    for (int device = 0; device < device_count; ++device) {
+        cudaDeviceProp device_prop;
+        cudaGetDeviceProperties(&device_prop, device);
+
+        std::cout << "Device " << device << ": " << device_prop.name << "\n";
+        std::cout << "  Max grid size: ("
+                  << device_prop.maxGridSize[0] << ", "
+                  << device_prop.maxGridSize[1] << ", "
+                  << device_prop.maxGridSize[2] << ")\n";
+    }
+}
+
+void check_max_block_size() {
+    int device_count = 0;
+    cudaGetDeviceCount(&device_count);
+
+    for (int device = 0; device < device_count; ++device) {
+        cudaDeviceProp device_prop;
+        cudaGetDeviceProperties(&device_prop, device);
+
+        std::cout << "Device " << device << ": " << device_prop.name << "\n";
+        std::cout << "  Max threads per block: " << device_prop.maxThreadsPerBlock << "\n";
+        std::cout << "  Max threads per block dimension: ("
+                  << device_prop.maxThreadsDim[0] << ", "
+                  << device_prop.maxThreadsDim[1] << ", "
+                  << device_prop.maxThreadsDim[2] << ")\n";
+    }
+}
